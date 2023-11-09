bd5535c0bb4a33cbcc17b037bf7057741d048b40
thong0128 <thong0128@gmail.com>
2023-11-09 14:49:26 +0700

    Add OperatorPractice.html and js file

---
+++ OperatorPractice.js
@@ -0,0 +1,6 @@
+let Width = prompt('Enter the width');
+let Height= prompt("Enter the height");
+let Area= Width * Height;
+// Width = parseInt(Width);
+// Height = parseInt(Height);
+document.write("The area is: " + Area)
